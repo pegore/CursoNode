ef7153b790fa7a08a481fa6f6c6dc06e0b774971
Lino Pegoretti <lino.pegoretti@gmail.com>
2019-03-29 20:37:39 -0300

    commit com mongoose e body-parser

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-//mongodb+srv://usuario_admin:<password>@cluster0-zurhs.mongodb.net/test?retryWrites=true
-
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const url = 'mongodb+srv://usuario_admin:<password>@cluster0-zurhs.mongodb.net/test?retryWrites=true';
+const options = { reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true };
+
+mongoose.connect(url, options);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connection.on('error', (err) => {
+    console.log('Erro na conexão com o banco de dados: ' + err);
+})
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Aplicação desconectada do banco de dados!');
+})
+
+mongoose.connection.on('connected', () => {
+    console.log('Aplicação conectada ao banco de dados!');
+})
+
+//BODY PARSER
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const indexRoute = require('./Routes/index');
 const usersRoute = require('./Routes/users');
@@ -10,6 +32,5 @@ app.use('/', indexRoute);
 app.use('/users', usersRoute);
 
 app.listen(3000);
-
 
 module.exports = app;
